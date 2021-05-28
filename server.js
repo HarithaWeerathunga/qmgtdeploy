19166d9c20bdd522cbcff8d097f30e040902ad54
HarithaWeerathunga <harithanasa@gmail.com>
2021-05-28 17:21:03 +0530

    develop

--- server.js
+++ server.js
@@ -8,12 +8,12 @@ const PORT = process.env.PORT || 8080;
 
 
 const MONGODB_URI = 'mongodb+srv://admin:admin@cluster0.7gwjm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-
+const localMongo = "mongodb://localhost/merndb";
 
 const routes = require('./routes/api');
 
 
-mongoose.connect(MONGODB_URI || 'mongodb://localhost/merndb', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/merndb', {
     useNewUrlParser: true,
     useUnifiedTopology: true 
 });
@@ -33,6 +33,12 @@ app.use(express.urlencoded({extended: false}));
 //http request logger
 app.use(morgan('tiny'));
 app.use('/api', routes)
+
+
+if(process.env.NODE_ENV=== 'production'){
+   app.use(express.static('client/build'));
+   
+}
 
 
 app.listen(PORT, console.log(`server is starting at ${PORT}`));
