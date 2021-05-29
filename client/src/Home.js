61a36a5598565ffbd47606dda3fb6e9dabddbf6f
HarithaWeerathunga <harithanasa@gmail.com>
2021-05-29 08:41:01 +0530

    added bootstrap google fonts and navbar

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,11 +1,14 @@
 import React, {Component} from 'react';
+import {Navbar, Nav, NavDropdown} from 'react-bootstrap';
 
 export default class Home extends Component{
 
     render(){
         return(
-            <div>
-                <h1>Home</h1>
+            <div className="App App-header">
+                <Navbar bg="primary" variant="dark" expand="x1">
+                    <Navbar.Brand>Queue Analytics</Navbar.Brand>
+                </Navbar>
             </div>
         )
     }
