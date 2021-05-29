ad38b7021f221e5df8e2fd34d76c21abd2d1f5b5
HarithaWeerathunga <harithanasa@gmail.com>
2021-05-29 10:53:28 +0530

    api settings

--- routes/api.js
+++ routes/api.js
@@ -47,47 +47,53 @@ router.get('/video/:id/data', (req, res) => {
     res.json(videos);
 });
 
-router.get('/video/:id', (req, res) => {
-    
-    console.log("/video/id/------jjjjjj---");
 
-    if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
 
-    const path = `assets/${req.params.id}.mp4`;
+    router.get('/video/:id', (req, res) => {
     
-    const stat = fs.statSync(path);
-    const fileSize = stat.size;
-    const range = req.headers.range;
+        console.log("/video/id/------jjjjjj---");
+    
+        
+    
+        const path = `assets/${req.params.id}.mp4`;
+        
+        const stat = fs.statSync(path);
+        const fileSize = stat.size;
+        const range = req.headers.range;
+    
+        
+        if (range) {
+            const parts = range.replace(/bytes=/, "").split("-");
+            const start = parseInt(parts[0], 10);
+            const end = parts[1]
+                ? parseInt(parts[1], 10)
+                : fileSize-1;
+            const chunksize = (end-start) + 1;
+            const file = fs.createReadStream(path, {start, end});
+            const head = {
+                'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+                'Accept-Ranges': 'bytes',
+                'Content-Length': chunksize,
+                'Content-Type': 'video/mp4',
+            };
+            res.writeHead(206, head);
+            file.pipe(res);
+        } else {
+            const head = {
+                'Content-Length': fileSize,
+                'Content-Type': 'video/mp4',
+            };
+            res.writeHead(200, head);
+            fs.createReadStream(path).pipe(res);
+        }
+    
+        
+        
+    });
 
-    
-    if (range) {
-        const parts = range.replace(/bytes=/, "").split("-");
-        const start = parseInt(parts[0], 10);
-        const end = parts[1]
-            ? parseInt(parts[1], 10)
-            : fileSize-1;
-        const chunksize = (end-start) + 1;
-        const file = fs.createReadStream(path, {start, end});
-        const head = {
-            'Content-Range': `bytes ${start}-${end}/${fileSize}`,
-            'Accept-Ranges': 'bytes',
-            'Content-Length': chunksize,
-            'Content-Type': 'video/mp4',
-        };
-        res.writeHead(206, head);
-        file.pipe(res);
-    } else {
-        const head = {
-            'Content-Length': fileSize,
-            'Content-Type': 'video/mp4',
-        };
-        res.writeHead(200, head);
-        fs.createReadStream(path).pipe(res);
-    }
+}
 
-    }
-    
-});
 
 
 
