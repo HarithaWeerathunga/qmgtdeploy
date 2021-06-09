b129a7ec128419d6b4f4def7bf687cb8bee40ef6
HarithaWeerathunga <harithanasa@gmail.com>
2021-06-09 15:25:48 +0530

    added another video

--- client/src/HomeGrid.js
+++ client/src/HomeGrid.js
@@ -113,13 +113,13 @@ export default class HomeGrid extends Component{
                                         <NavDropdown.Divider/>
                                         <NavDropdown.Item href="/4">Video 4</NavDropdown.Item>
                                         <NavDropdown.Divider/> */}
-                                        <NavDropdown.Item> <Link to="/0" >Camera 1</Link></NavDropdown.Item>
+                                        <NavDropdown.Item> <Link to="/1" >Camera 1 </Link> </NavDropdown.Item>
                                         <NavDropdown.Divider/>
-                                        <NavDropdown.Item> <Link to="/1" >Camera 2 </Link> </NavDropdown.Item>
-                                        <NavDropdown.Divider/>
-                                        <NavDropdown.Item> <Link to="/2" >Camera 3 </Link></NavDropdown.Item>
+                                        <NavDropdown.Item> <Link to="/2" >Camera 2 </Link></NavDropdown.Item>
                                         <NavDropdown.Divider/>
                                         <NavDropdown.Item> <Link to="/4" >Camera 4 </Link></NavDropdown.Item>
+                                        <NavDropdown.Divider/>
+                                        <NavDropdown.Item> <Link to="/8" >Camera 8 </Link></NavDropdown.Item>
                                         <NavDropdown.Divider/>
                                         
                                    
